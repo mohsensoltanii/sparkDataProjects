4f1845a5b172a3edf7dbf53e22bc557433be11e6
Mohsen Soltani <soltanidv@gmail.com>
2023-10-21 13:42:48 +0330

    it's second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Spark world!");
+        System.out.println("Hello Spark Data world!");
     }
 }
